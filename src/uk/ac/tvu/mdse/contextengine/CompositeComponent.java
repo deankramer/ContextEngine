5e8d7db864d2804ca670724bb439d75397d906bf
Dean Kramer <dean.kramer@uwl.ac.uk>
2011-06-05 22:09:51 +0100

    added negative contexts to composite and checking logic added either contexts, but need to add logic

--- src/uk/ac/tvu/mdse/contextengine/CompositeComponent.java
+++ src/uk/ac/tvu/mdse/contextengine/CompositeComponent.java
@@ -17,22 +17,28 @@ public class CompositeComponent extends Component implements Serializable{
 	private static final long serialVersionUID = -493221379660180723L;
 	//Attributes
 	public Hashtable<String, Boolean> positivecontexts;
+	public Hashtable<String, Boolean> negativecontexts;
+	public Hashtable<String, Boolean> eithercontexts;
+	public Boolean eithercontextvalue;
 	
 	public CompositeComponent(String name, Context c){
 		super(name, c);
 		positivecontexts = new Hashtable<String, Boolean>();
+		negativecontexts = new Hashtable<String, Boolean>();
+		eithercontexts = new Hashtable<String, Boolean>();
+		eithercontextvalue=false;
 		setupMonitor();
 		
 	}
 	
-	public CompositeComponent(String name, Context c, ArrayList<String> contextList){
+	public CompositeComponent(String name, Context c, ArrayList<String> pc, ArrayList<String> nc){
 		super(name, c);
 		positivecontexts = new Hashtable<String, Boolean>();
 		setupMonitor();
-		for(String cn : contextList){
-			registerComponent(cn);
-		}
-		
+		for(String cn : pc)
+			registerComponent(cn, false);
+		for(String cn : nc)
+			registerComponent(cn, true);
 	}
 
 	private void setupMonitor() {
@@ -47,24 +53,58 @@ public class CompositeComponent extends Component implements Serializable{
 			if(positivecontexts.containsKey(context)){
 					positivecontexts.put(context, value);
 					checkContext();	
-			}	
+			}
+			
+			if(negativecontexts.containsKey(context)){
+					negativecontexts.put(context, value);
+					checkContext();
+			}
+			
+			if(eithercontexts.containsKey(context)){
+					eithercontexts.put(context, value);
+					checkContext();
+			}
 		}};
 		context.registerReceiver(contextMonitor, filter);
 		
 	}
 	
-	public boolean registerComponent(String c){
-		if ( ! positivecontexts.containsKey(c)){	
-			positivecontexts.put(c, false);
-			return true;		
-		}else
-			return false;
+	public boolean registerComponent(String c, Boolean v){
+		if(v){
+			if ( ! positivecontexts.containsKey(c)){	
+				positivecontexts.put(c, !v);
+				return true;		
+			}else
+				return false;
+		}else{
+			if (! negativecontexts.containsKey(c)){
+				negativecontexts.put(c, !v);
+				return true;
+			}else 
+				return false;
+		}
+	}
+	
+	public boolean registerComponent(ArrayList<String> contextList, Boolean value){
+		
+		if(contextList.size()>0){
+			eithercontextvalue=value;
+			for(String c : contextList){
+				eithercontexts.put(c, ! value);
+			}
+			return true;
+		}
+		return false;
 	}
 	
 	public boolean unregisterComponent(String c){
 		
 		if (positivecontexts.containsKey(c)){
 			positivecontexts.remove(c);
+			return true;
+		}
+		else if (negativecontexts.containsKey(c)){
+			negativecontexts.remove(c);
 			return true;
 		}else
 			return false;	
@@ -78,11 +118,13 @@ public class CompositeComponent extends Component implements Serializable{
 	}
 	
 	public void checkContext(){
-		if ( ! positivecontexts.containsValue(false) && contextValue==false){
+		if ( ( ! positivecontexts.containsValue(false)) && (! negativecontexts.containsValue(true)) 
+				&& contextValue==false){
 			sendNotification(true);
 			contextValue=true;
 		}
-		else if (positivecontexts.containsValue(false) && contextValue==true){
+		else if (positivecontexts.containsValue(false) || negativecontexts.containsValue(false) 
+				&& contextValue==true){
 			sendNotification(false);
 			contextValue=false;
 		}
