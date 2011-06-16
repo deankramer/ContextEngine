eee2f36c409d5ff283bd81da56414105b3980512
Dean Kramer <dean.kramer@uwl.ac.uk>
2011-06-16 11:42:07 +0100

    Context checks its telephony connection state

--- src/uk/ac/tvu/mdse/contextengine/contexts/TelephonyContext.java
+++ src/uk/ac/tvu/mdse/contextengine/contexts/TelephonyContext.java
@@ -5,14 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.telephony.TelephonyManager;
-import android.util.Log;
 import uk.ac.tvu.mdse.contextengine.Component;
 
 public class TelephonyContext extends Component {
 
 	private boolean roaming = false;
+	private int connection = 0;
 	private static final long serialVersionUID = -6310232951343360172L;
-	private TelephonyManager tm;
+	public TelephonyManager tm;
 
 	public TelephonyContext(String name, Context c) {
 		super("TELEPHONY", c);
@@ -36,7 +36,20 @@ public class TelephonyContext extends Component {
 	protected void checkContext() {
 		// TODO Auto-generated method stub
 		checkRoaming();
+		checkConnectionState();
 
+	}
+
+	private void checkConnectionState() {
+		int v = tm.getDataState();
+		if (v != connection){
+			if(v == 2)
+				sendNotification("telephonyConnectedON", true);
+			else
+				sendNotification("telephonyConnectedON", false);
+			connection = v;
+		}
+		
 	}
 
 	private void checkRoaming() {
