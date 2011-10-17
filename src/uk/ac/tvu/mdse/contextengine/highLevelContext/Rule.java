c44413a47af9cbdacd7af11cf0043c0c08e62491
Dean Kramer <dean.kramer@uwl.ac.uk>
2011-10-17 01:13:23 +0100

    Updated Licensing, adder headers to source

--- src/uk/ac/tvu/mdse/contextengine/highLevelContext/Rule.java
+++ src/uk/ac/tvu/mdse/contextengine/highLevelContext/Rule.java
@@ -1,12 +1,22 @@
+/*
+ * Copyright (C) 2011 The Context Engine Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.tvu.mdse.contextengine.highLevelContext;
 
 import android.util.Log;
-
-/**
- * @project ContextEngine
- * @date 28 Jul 2011
- * @author Dean Kramer & Anna Kocurova
- */
 
 public class Rule {
 
